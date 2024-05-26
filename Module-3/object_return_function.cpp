6fa35bdaa886d5ad7680f8b3ac86898f1110272e
Sagor Ahmed <121637675+developersagorahmed@users.noreply.github.com>
2024-05-26 10:23:02 +0600

    object return from function static

---
+++ Module-3/object_return_function.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+    Student(int roll, int cls, double gpa){
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+Student *fun(){
+    Student rahim(10, 12, 3.47);
+    Student *p = &rahim;
+    return p;
+}
+
+int main(){
+    
+    Student *ans = fun();
+
+    // cout << ans.roll << " " << ans.cls<< " " << ans.gpa<<ends;
+    cout<< ans->roll << " " << ans->cls << " " << ans->gpa << endl;
+    return 0;
+}
