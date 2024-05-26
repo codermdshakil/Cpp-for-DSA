69b4ec65984e8834b3cd123b12c84f2ece654ba1
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-26 12:12:25 +0600

    comparison problem solved

--- Module-3.5/V_Comparison.cpp
+++ Module-3.5/V_Comparison.cpp
@@ -1,7 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    char inputChar;
+    cin >> inputChar;
+    int b;
+    cin >> b;
 
-int main(){
-    cout << "Inisialise practice day";
+
+
+    
+    // Check if the input character is '>'
+    if (inputChar == '>')
+    {
+        if (a > b)
+        {
+            cout << "Right" << endl;
+        };
+    }
+    else if (inputChar == '<') // Check if the input character is '<'
+    {
+        if (b < a)
+        {
+            cout << "Wrong" << endl;
+        }
+    }
+    else if (inputChar == '=') // Check if the input character is '='
+    {
+        if (a = b)
+        {
+            cout << "Right" << endl;
+        }
+    }
+
     return 0;
 }
