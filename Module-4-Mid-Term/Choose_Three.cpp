532add0ab44aad30e6859ec1a7ad3c16cba9972a
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-30 22:59:05 +0600

    Choose three math problem solved

--- Module-4-Mid-Term/Choose_Three.cpp
+++ Module-4-Mid-Term/Choose_Three.cpp
@@ -1,11 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int can_find_triplet_with_sum(int *arr, int n, int S)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            for (int k = j + 1; k < n; ++k)
+            {
+                if (arr[i] + arr[j] + arr[k] == S)
+                {
+                    return S;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 int main()
 {
+
     int test;
     cin >> test;
-
     for (int i = 0; i < test; i++)
     {
         int n, sum;
@@ -17,31 +35,7 @@ int main()
             cin >> ar[i];
         }
 
-        // array reverse
-        sort(ar, ar + n, greater<int>());
-        
-        int first_num, second_num, third_num, first_second_sum;
-
-        for (int i = 0; i < 5; i++)
-        {
-            first_num = ar[0];
-
-            for (int j = 1; j < n; j++)
-            {
-                second_num = ar[1];
-                first_second_sum = first_num + second_num;
-
-                for (int k = 2; k < n; k++)
-                {
-                    if (first_second_sum + ar[k] == sum)
-                    {
-                        first_second_sum = first_second_sum + ar[k];
-                    }
-                }
-            }
-        }
-
-        if (first_second_sum == sum)
+        if (can_find_triplet_with_sum(ar, n, sum))
         {
             cout << "YES" << endl;
         }
