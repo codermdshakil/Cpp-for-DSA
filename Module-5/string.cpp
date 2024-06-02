8fa95d9a6cf0ccb3570704b0ae30483d637c3ba2
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-02 22:19:49 +0600

    String declare in c++

---
+++ Module-5/string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // declare a string and assign value
+    string s = "Shakil Ahmed";
+
+    string s1 = "Hello";
+    string s2 = "hello";
+
+    
+    return 0;
+}
