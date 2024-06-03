52f6a5b96eda54de1cd0a5b3ae156118c057a5b2
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-03 09:59:25 +0600

    modifie string using - a=a+b, s+=, a.append, a.push_back, a.pop_back

---
+++ Module-5/string_modifiers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string a = "Hello";
+    string b = "World";
+
+    // a= a+b;
+    // a+=b; // append string to this string
+    // a.append(b); // inside of a append b
+
+    // append character in last index of string
+    // a = a + 'X';
+    // a.push_back('X'); // HelloX
+
+    // remove character in last index of string
+    // a.pop_back(); // Hello
+
+    cout << a << endl; // HelloWorld
+    return 0;
+}
