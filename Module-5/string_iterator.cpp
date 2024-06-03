38a5bd1b6a2cc470e61c730dd3786fec187cd9a7
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-03 11:16:48 +0600

    using iterator print string

--- Module-5/string_iterator.cpp
+++ Module-5/string_iterator.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
     string s;
-    cin>>s;
+    cin >> s;
 
-    for (auto it = s.begin(); it < s.end();it++)
+    // cout << *s.begin() << endl; // first char pointer
+    // cout << *(s.end()-1) << endl; // last char pointer
+
+    // create a new iterator
+    // string::iterator it;
+    // string::iterator it = auto 
+    // auto just can use c++ version 12
+    for (auto it =  s.begin(); it < s.end(); it++)
     {
         cout << *it << " ";
     }
-    
+
     return 0;
 }
