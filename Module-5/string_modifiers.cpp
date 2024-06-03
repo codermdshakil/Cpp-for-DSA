c96d1dfe020269c5229b578992cc8a861c659da9
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-03 10:41:40 +0600

    string erase , string replace, string insert

--- Module-5/string_modifiers.cpp
+++ Module-5/string_modifiers.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
 
-    string a = "Hello";
-    string b = "World";
+    // string a = "Hello";
+    // string b = "World";
 
     // a= a+b;
     // a+=b; // append string to this string
@@ -18,6 +18,29 @@ int main()
     // remove character in last index of string
     // a.pop_back(); // Hello
 
-    cout << a << endl; // HelloWorld
+    // Assign
+    // a = "gelo";
+    // a.assign("Gelo");
+
+    // from string remove a portion
+    //----------------------------- 
+    // string a = "Helloworld";
+    // a.erase(4); // if i sent just one parameter Including this number remove all character in string
+    //a.erase(4, 3); // Hellworld  // first parameter start index, second parameter contain how much character You want to delete
+
+    // replace = erase+append
+    //------------------------
+    // string a = "HelloWorld";
+    // a.replace(4,3,"eye"); // 4,3 means - erase this part, "eye" means - replace with this
+    // cout << a << endl; // Helleyerld
+
+
+    // Insert - insert a portion in a specific portion in array
+    //------------------------
+    string a = "HelloWorld";
+    // a.replace(5,0,"Shakil");
+    // a.insert(5, "Shakil"); 
+    cout << a << endl; // HelloShakilWorld
+
     return 0;
 }
