c54d67f1935a684a78a1fe3d4b5dd789ecd1681f
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-03 11:57:40 +0600

    string input with space

---
+++ Module-5/string_input_with_space.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x;
+    cin >> x ;
+    // cin.ignore(); //just for C++ // Iqnore one character
+    getchar(); // C/c++ both
+    string s;
+    // cin>>s; // without space
+    getline(cin, s); // with space 
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
