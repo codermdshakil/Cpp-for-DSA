6ae4ca07fa656dd59ea8e18d91a21d78e45ff367
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-03 09:40:35 +0600

    string access built in function - s[i], s.at[i], s.front, s.back

---
+++ Module-5/string_element_access.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    // print string character by character
+    for (int i = 0; i < s.size(); i++)
+    {
+        // cout << s[i] << " ";
+        // cout << s.at(i) << " "; - same like s[i]
+    }
+
+    // access first element of string
+    // cout << s.front();  // H
+
+    // access last element of string
+    // cout << s.back();  // d
+ 
+
+    return 0;
+}
