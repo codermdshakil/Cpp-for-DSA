dcb5ad5c7562ccab420f5d1aff61b7faf6444ef6
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-04 13:33:52 +0600

    String constructor

---
+++ Module-6/string_constructor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // string s = "Hello world";
+    // Case 1
+    // string s("Hello world");
+
+    // Case 2
+    // string s("Hello world", 5); // 5 means print 0 index to 5 // - Hello
+
+    // Case 3
+    // string a = "Hello world";
+    // string s(a, 5); // 5 means after 5 index print all char // -  world
+    // Case 4
+    string s(10, 'S'); // 10 means print S 10 times
+
+    cout << s << endl;
+
+    return 0;
+}
