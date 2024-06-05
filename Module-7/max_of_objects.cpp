bb3f13600818d7994b7e2b5f16ad6b0e4137dd6d
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-05 14:43:25 +0600

    max value from objects

---
+++ Module-7/max_of_objects.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+
+    // find max
+    int mx = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+        // check
+        if (a[i].marks > mx)
+        {
+            mx = a[i].marks;
+        }
+    }
+
+    // print min
+    cout << mx;
+
+    return 0;
+}
