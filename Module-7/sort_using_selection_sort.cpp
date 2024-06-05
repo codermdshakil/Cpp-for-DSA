3bfae09cdbf8fef2ee1eeb286c8c98cac1f498f4
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-05 23:42:43 +0600

    using selection sort sorted assending order objects

---
+++ Module-7/sort_using_selection_sort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+
+    // selection sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // assending order based on marks
+            if (a[i].marks > a[j].marks)
+            {
+                // called swap function
+                swap(a[i], a[j]);
+            }
+        }
+    }
+
+    // print assending order baded on marks sorted objects
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+
+    return 0;
+}
